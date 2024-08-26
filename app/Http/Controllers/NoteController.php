8208ba71cfc791cf28ee2fa5fc5bec541f86adc9
rodlez <xavier.rodlez@gmail.com>
2024-08-26 11:17:24 +0200

    Render the DB content in index

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -12,7 +12,9 @@ class NoteController extends Controller
      */
     public function index()
     {
-        return view('note.index');
+        $notes = Note::query()->orderBy('created_at', 'desc')->paginate(10);
+        //dd($notes);
+        return view('note.index', ['notes' => $notes]);
     }
 
     /**
@@ -36,7 +38,7 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
-        return view('note.show');
+        return view('note.show', ['note' => $note]);
     }
 
     /**
@@ -44,7 +46,7 @@ class NoteController extends Controller
      */
     public function edit(Note $note)
     {
-        return view('note.edit');
+        return view('note.edit', ['note' => $note]);
     }
 
     /**
--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,4 +1,26 @@
 <!-- To use the layout.blade.php in views/components as a Template to render in the slot variable -->
 <x-layout>
-    <h1>index</h1>
+    <!-- Notes -->    
+    <div class="note-container">
+        <!-- 2 ways to generate the routes, hardcoded or use the name defined in the routes -->
+        <a href="note/create" class="new-note-btn">Create Note</a>
+        <a href="{{ route('note.create') }}" class="new-note-btn">Create Note</a>
+        <div class="notes">
+            @foreach ($notes as $note)
+            <div class="note">
+                <div class="note-body">
+                {{ Str::words($note->note, 30) }}
+                </div>
+                <div class="note-buttons">
+                    <!-- To include the id -> Laravel resolve note passing the id key if we include the object, or we can pass it specifically -->
+                    <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
+                    <a href="{{ route('note.edit', $note->id) }}" class="note-edit-button">Edit</a>
+                    <button class="note-delete-button">Delete</button>
+                </div>
+            </div>
+            @endforeach
+            
+        </div>
+    </div>
+
 </x-layout>
